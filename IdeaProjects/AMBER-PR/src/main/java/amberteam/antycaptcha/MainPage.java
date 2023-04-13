850ac5a7281a47ca871973ab5c5e4431383b49fe
Matey <matey4@poczta.onet.pl>
2023-04-13 12:20:18 +0200

    logi 2

--- IdeaProjects/AMBER-PR/src/main/java/amberteam/antycaptcha/MainPage.java
+++ IdeaProjects/AMBER-PR/src/main/java/amberteam/antycaptcha/MainPage.java
@@ -1,27 +1,26 @@
 package amberteam.antycaptcha;
 
 import com.microsoft.playwright.Page;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class MainPage {
 
     Page page;
-    Logger logger = LoggerFactory.getLogger(MainPage.class);
-
+    static Logger logger = LoggerFactory.getLogger(MainPage.class);
 
     public MainPage(Page page) {
         this.page = page;
     }
 
     public static void openMainPage(Page page) {
-        System.out.println("User opens antycaptcha.amberteam main page.");
+        logger.info("User opens antycaptcha.amberteam main page.");
         page.navigate("http://antycaptcha.amberteam.pl/");
     }
 
     public void openExercise(int exerciseNumber) {
-        System.out.println("User opens Exercise"+exerciseNumber+" page");
+        logger.info("User opens Exercise"+exerciseNumber+" page");
         page.click("//a[contains(@href,'exercise" + exerciseNumber + "')]");
     }
 }
